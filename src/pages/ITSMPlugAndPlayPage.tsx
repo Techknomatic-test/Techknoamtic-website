0e492886cf071ac0f5c27da17067dc1dfe44fa51
techknomatic-vinod <vinod.tambe@techknomatic.com>
2026-05-25 16:52:20 +0530

    orange-removed

--- src/pages/ITSMPlugAndPlayPage.tsx
+++ src/pages/ITSMPlugAndPlayPage.tsx
@@ -529,7 +529,7 @@ export const ITSMPlugAndPlayPage = () => {
             </motion.div>
 
             <div className="space-y-6 self-start lg:pt-4">
-              <h4 className="mb-8 text-[14px] font-black uppercase tracking-widest text-accent">
+              <h4 className="mb-8 text-[14px] font-black uppercase tracking-widest">
                 Common Operational Pain Points
               </h4>
               {[
